9722bf20e24c58eea686cf9b9cc4493dca1e7321
Kevin Oberlies <favilo@gmail.com>
2022-10-04 08:41:04 -0700

    More paths

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import init, { run_app } from './pkg/yew_wasm_pack_minimal.js';
+import init, { run_app } from '/simon-at-simon/pkg/yew_wasm_pack_minimal.js';
 async function main() {
    await init('/simon-at-simon/pkg/yew_wasm_pack_minimal_bg.wasm');
    run_app();
